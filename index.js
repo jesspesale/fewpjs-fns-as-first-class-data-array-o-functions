75e1ad6841038b32b8a47477a3aad7a330c43010
Jess Pesale <jess_pesale@aol.com>
2021-07-08 16:56:04 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function wakeDog(dogName, dogBreed) {
+  const wake = console.log(`Wake ${dogName} the ${dogBreed}`);
+  return wake
+}
+
+function leashDog(dogName, dogBreed) {
+    const leash = console.log(`Leash ${dogName} the ${dogBreed}`)
+    return leash
+}
+
+function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`)
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
+}
+
+function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`)
+}
+
+function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`)
+}
